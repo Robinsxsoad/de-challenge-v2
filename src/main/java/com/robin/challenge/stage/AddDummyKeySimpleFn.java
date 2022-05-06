bb6ca6572c197c6becdd3d3378307af527208072
Robin <robinson.oyarzun@gmail.com>
2022-05-05 20:30:54 -0400

    CHG: code style

--- src/main/java/com/robin/challenge/stage/AddDummyKeySimpleFn.java
+++ src/main/java/com/robin/challenge/stage/AddDummyKeySimpleFn.java
@@ -7,7 +7,7 @@ import org.apache.beam.sdk.values.KV;
 /**
  * Function to add a dummy key to the original PCollection of String and season stats. Adding this dummy key will allow the next step in the pipeline to iterate the PCollection elements.
  */
-public class AddDummyKeySimpleFn extends SimpleFunction<KV<String, SeasonTeamStats>, KV<String, KV<String, SeasonTeamStats>>>{
+public class AddDummyKeySimpleFn extends SimpleFunction<KV<String, SeasonTeamStats>, KV<String, KV<String, SeasonTeamStats>>> {
 
     @Override
     public KV<String, KV<String, SeasonTeamStats>> apply(KV<String, SeasonTeamStats> input) {
